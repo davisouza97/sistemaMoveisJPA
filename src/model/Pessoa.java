709ccf6a40fdfddd2e321619fa09770081de3926
davisouza97 <davisouza1997jf@gmail.com>
2019-03-28 13:14:28 -0300

    Model ~aparentemente~ feito

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,9 +1,13 @@
 package model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Pessoa {
     private static final long serialVersionUID = 1L;
     @Id
