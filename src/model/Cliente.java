695aaf37d3de026cae92fc5ff8e1c422841b30c6
WalterResende85 <walterccr@hotmail.com>
2019-04-03 12:42:43 -0300

    tirado id do cliente

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -9,7 +9,7 @@ import javax.persistence.Entity;
 @Entity
 public class Cliente extends Pessoa implements Serializable {
 
-    public Long id;
+    
 
     public Cliente(String nome, String cpf, String dataNascimento, String email, String cep, String logradouro, String numero, String complemento, String bairro, String uf, String cidade, String telefone, String celular) {
         super(nome, cpf, dataNascimento, email, cep, logradouro, numero, complemento, bairro, uf, cidade, telefone, celular);
