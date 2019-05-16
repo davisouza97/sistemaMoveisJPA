a9b5c7a8e71ec40b7a0f466a17015b350b6d3985
davisouza97 <davisouza1997jf@gmail.com>
2019-05-16 12:41:38 -0300

    lknm,

--- src/controller/ManterFornecedorController.java
+++ src/controller/ManterFornecedorController.java
@@ -5,9 +5,7 @@
  */
 package controller;
 
-import dao.FornecedorDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
--- src/controller/PesquisaMovelController.java
+++ src/controller/PesquisaMovelController.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
