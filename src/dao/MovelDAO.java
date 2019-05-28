048ea5aaafa3257d7b03db5749147e500c2b5658
davisouza97 <davisouza1997jf@gmail.com>
2019-05-28 17:49:58 -0300

    aeeeee
    aeee

--- src/dao/MovelDAO.java
+++ src/dao/MovelDAO.java
@@ -17,25 +17,4 @@ public class MovelDAO extends GeralDAO{
 
     private MovelDAO() {
     }
-
-    public List<Movel> findAllByPedido(Pedido pedido) {
-        EntityManager em = PersistenceUtil.getEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        List<Movel> moveis = null;
-        try {
-            tx.begin();
-            TypedQuery<Movel> query = em.createQuery("select p from Movel p", Movel.class);
-            moveis = query.getResultList();
-            tx.commit();
-        } catch (Exception e) {
-            if (tx != null && tx.isActive()) {
-                tx.rollback();
-            }
-            throw new RuntimeException(e);
-        } finally {
-            PersistenceUtil.close(em);
-        }
-        return moveis;
-    }
-
 }
