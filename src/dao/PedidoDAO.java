289ea4b80fafb054d792f6c6a7d4e560a1e7fc47
WalterResende85 <walterccr@hotmail.com>
2019-05-28 18:37:44 -0300

    credão

--- src/dao/PedidoDAO.java
+++ src/dao/PedidoDAO.java
@@ -21,43 +21,5 @@ public class PedidoDAO extends GeralDAO{
     private PedidoDAO() {
     }
 
-    public Pedido find(Long id) {
-        EntityManager em = PersistenceUtil.getEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        Pedido pedido = null;
-        try {
-            tx.begin();
-            pedido = em.find(Pedido.class, id);
-            tx.commit();
-        } catch (Exception e) {
-            if (tx != null && tx.isActive()) {
-                tx.rollback();
-            }
-            throw new RuntimeException(e);
-        } finally {
-            PersistenceUtil.close(em);
-        }
-        return pedido;
-    }
-
-    public List<Pedido> findAll() {
-        EntityManager em = PersistenceUtil.getEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        List<Pedido> pedidos = null;
-        try {
-            tx.begin();
-            TypedQuery<Pedido> query
-                    = em.createQuery("select p from Pedido p", Pedido.class);
-            pedidos = query.getResultList();
-            tx.commit();
-        } catch (Exception e) {
-            if (tx != null && tx.isActive()) {
-                tx.rollback();
-            }
-            throw new RuntimeException(e);
-        } finally {
-            PersistenceUtil.close(em);
-        }
-        return pedidos;
-    }
+    
 }
